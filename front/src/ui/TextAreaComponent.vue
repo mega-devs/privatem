d7595d399863023e1b5c02a95deba15c14344d05
Yakup <yakup2000002@gmail.com>
2024-11-23 17:23:28 +0500

    new_ui_components

---
+++ front/src/ui/TextAreaComponent.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+  modelValue: String,
+  placeholder: {
+    type: String,
+    default: 'Enter text',
+  },
+  rows: {
+    type: Number,
+    default: 4,
+  },
+  classNames: {
+    type: String,
+    default: 'text-area',
+  },
+  isFull: {
+    type: Boolean,
+    default: false,
+  },
+  isRequired: {
+    type: Boolean,
+    default: false,
+  }
+})
+
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <textarea
+      :placeholder="placeholder"
+      :rows="rows"
+      :class="[classNames, { 'w-100': isFull }]"
+      :value="modelValue"
+      :required="isRequired"
+      @input="$emit('update:modelValue', $event.target.value)"
+  ></textarea>
+</template>
+
+<style scoped>
+</style>
